cb8efa2591267be478b1e12c24b045b3a4be82cf
Yesna-Omar <109865751+Yesna-Omar@users.noreply.github.com>
2024-02-05 20:57:24 +0000

    installing-aos

---
+++ app/src/Component/RightSide.jsx
@@ -0,0 +1,21 @@
+// import React, { useEffect } from 'react';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+
+
+function RightSide() {
+
+  // useEffect(() => {
+  //   AOS.init(); 
+  // }, []);
+
+  return (
+    <div className="RightSide">
+     <div class="main-pic">
+      {/* <h1 data-aos="fade-in">Yesna Omar</h1> */}
+     </div>
+    </div>
+  );
+}
+
+export default RightSide;
